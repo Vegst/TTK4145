0c4a65469cc4195d598efa2b9b162ee2177fb180
AllawiT <fredrvaa@stud.ntnu.no>
2017-03-01 15:48:20 +0100

    Event manager - minor changes

--- Project/driver/IO.go
+++ Project/driver/IO.go
@@ -1,4 +1,4 @@
-package driver  // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
+package driver // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
 /*
 #cgo CFLAGS: -std=c11
 #cgo LDFLAGS: -lcomedi -lm
@@ -9,28 +9,28 @@ import (
 	"fmt"
 )
 
-func IO_init() bool{
+func IO_init() bool {
 	init := int(C.io_init())
 	fmt.Println(init)
 	return bool(init != -1)
 }
 
-func IO_set_bit(channel int){
+func IO_set_bit(channel int) {
 	C.io_set_bit(C.int(channel))
 }
 
-func IO_clear_bit(channel int){
+func IO_clear_bit(channel int) {
 	C.io_clear_bit(C.int(channel))
 }
 
-func IO_read_bit(channel int) bool{
+func IO_read_bit(channel int) bool {
 	return bool(int(C.io_read_bit(C.int(channel))) != 0)
 }
 
-func IO_write_analog(channel int, value int){
+func IO_write_analog(channel int, value int) {
 	C.io_write_analog(C.int(channel), C.int(value))
 }
 
-func IO_read_analog(channel int) int{
+func IO_read_analog(channel int) int {
 	return int(C.io_read_analog(C.int(channel)))
 }
--- Project/main2.go
+++ Project/main2.go
@@ -1,27 +1,32 @@
 package main
 
-import(
+import (
 	"./driver"
+	"time"
 )
 
 func main() {
-	driver.Elev_init()
-	driver.Elev_set_motor_direction(driver.DIRN_UP)
+
+	bCh := make(chan driver.ButtonEvent)
+	lCh := make(chan driver.LightEvent)
+	sCh := make(chan bool)
+	msCh := make(chan driver.Elev_motor_direction_t)
+	fCh := make(chan int)
+
+	msCh <- driver.DIRN_UP
+
+	go driver.Elevator_event_manager(bCh, lCh, sCh, msCh, fCh)
 
 	for {
-        if (driver.Elev_get_floor_sensor_signal() == driver.N_FLOORS - 1) {
-            driver.Elev_set_motor_direction(driver.DIRN_DOWN)
-        } else if (driver.Elev_get_floor_sensor_signal() == 0) {
-            driver.Elev_set_motor_direction(driver.DIRN_UP)
-        }
-        c := make(chan driver.Order, 1)
-        go driver.OrderHandler_get_orders(c)
-
-
-        // Stop elevator and exit program if the stop button is pressed
-        if (driver.Elev_get_stop_signal()) {
-            driver.Elev_set_motor_direction(driver.DIRN_STOP)
-            return
-        }
-    }
+		select {
+		case fi := <-fCh:
+			if fi == driver.N_FLOORS-1 {
+				msCh <- driver.DIRN_DOWN
+			} else if fi == 0 {
+				msCh <- driver.DIRN_UP
+			}
+		default:
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
 }
