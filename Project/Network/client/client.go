a88a3d2e081985c4acc570d9fe4e6ba2e398c154
Unknown <vegardnittervestad@gmail.com>
2017-03-03 14:40:03 +0100

    Easy commit

--- Project/Network/client/client.go
+++ Project/Network/client/client.go
@@ -1,49 +1,77 @@
-package slave
+package client
  
 import (
     "fmt"
     "net"
-    "os"
+    "time"
 )
  
+type Event int
+const (
+	EVENT_CONNECTED = 1
+	EVENT_DISCONNECTED = 2
+)
+func ToString(event Event) string {
+	switch event {
+	case EVENT_CONNECTED:
+		return "Connected"
+	case EVENT_DISCONNECTED:
+		return "Disconnected"
+	}
+	return "Unknown"
+}
+
 type Message struct {
 	Data string
 }
 
-func Client(ip string, port int, id string, enableCh <-chan bool, sendCh <-chan Message, receiveCh chan Message, eventCh chan Event) {
+func Client(port int, serverCh <-chan string, sendCh <-chan Message, receiveCh chan Message, eventCh chan Event) {
 
-	var connection net.Listener
+	var connection net.Conn
 	var err error
 	connected := false
-	enabled := false
+	server := ""
 
 	for {
 		select {
-		case enabled = <-enableCh:
+		case server = <-serverCh:
 		case message := <-sendCh:
 			if connected {
-				_,err := client.Write([]byte(message.Data))
+				_,err := connection.Write([]byte(message.Data))
 				if err != nil {
 					fmt.Println(err)
 				}
 			}
 		default:
 			// Connect
-			if enabled && ! connected {
-				connection, err = net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
-				if err == nil {
-					connected = true
+			if server != "" {
+				if !connected {
+					connection, err = net.Dial("tcp", fmt.Sprintf("%s:%d", server, port))
+					if err == nil {
+						connected = true
+						eventCh <- EVENT_CONNECTED
+					}
 				}
 			// Disconnect
-			} else { 
+			} else if connected { 
 				connection.Close()
 				connected = false
+				eventCh <- EVENT_DISCONNECTED
+			}
+			// Alive
+			if connected {
+				buffer := make([]byte, 1024)
+
+				n, err := connection.Read(buffer)
+				if err == nil {
+					receiveCh <- Message{string(buffer[0:n])}
+				} else {
+					connection.Close()
+					connected = false
+					eventCh <- EVENT_DISCONNECTED
+				}
 			}
 			time.Sleep(50*time.Millisecond)
 		}
 	}
 }
-
-
-
-
--- Project/Network/test/slave.go
+++ Project/Network/test/slave.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"./client"
+	"../client"
 	"fmt"
 	"time"
 )
