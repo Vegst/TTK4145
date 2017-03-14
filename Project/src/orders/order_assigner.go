7c14e227ee4798ed58a69f1578ac7b6ccf7f5a84
Fredrik <fredrvaa@stud.ntnu.no>
2017-03-14 01:51:37 +0100

    Modified the order assigner so it returns time it takes for an elevator to reach a floor. Could need some more work with optimization of the code.

--- Project/src/orders/order_assigner.go
+++ Project/src/orders/order_assigner.go
@@ -3,28 +3,58 @@ package orders
 import (
 	. "../def"
 	"math"
+	"fmt"
 )
 
-func CalculateCost(o Order, e Elevator) float64 {
-	cost := float64(0)
-	cost += math.Abs(float64((o.Floor - e.State.Floor) * 10))
-
-	//Adds cost for each f
-	for i := 0; i < NumFloors; i++ {
-		for j := 0; j < NumTypes; j++ {
-			if e.Orders[i][j] {
-				cost += 3
+func numOrdersBelowToFloor(e Elevator, floor int) int{
+	numOrders := 0
+	for f := floor; f < e.State.Floor; f++{
+		for o := 0; 0 < NumTypes; o++{
+			if e.Orders[f][o] {
+				numOrders += 1
 			}
 			break
 		}
 	}
+	return numOrders
+}
 
-	if o.Floor < e.State.Floor && e.State.Direction == DirnUp ||
-		o.Floor > e.State.Floor && e.State.Direction == DirnDown {
-		cost += 20
+func numOrdersAboveToFloor(e Elevator, floor int) int{
+	numOrders := 0
+	for f := e.State.Floor; f < floor; f++{
+		for o := 0; 0 < NumTypes; o++{
+			if e.Orders[f][o] {
+				numOrders += 1
+			}
+			break
+		}
 	}
-	return cost
+	return numOrders
+}
 
+func CalculateCost(o Order, e Elevator) float64 {
+	const TimeBetweenFloors = 2
+
+	cost := 0
+
+	if(e.State.Direction == DirnDown && o.Floor >= e.State.Floor){
+		cost += 3 * numOrdersBelowToFloor(e, 0)
+		cost += ((e.State.Floor - 0) * TimeBetweenFloors)*2
+		cost += 3 * numOrdersAboveToFloor(e, o.Floor)
+		cost += (o.Floor - e.State.Floor) * TimeBetweenFloors
+	} else if(e.State.Direction == DirnUp && o.Floor < e.State.Floor){
+		cost += 3 * numOrdersAboveToFloor(e, NumFloors)
+		cost += ((NumFloors - e.State.Floor) * TimeBetweenFloors)*2
+		cost += 3 * numOrdersBelowToFloor(e, o.Floor)
+		cost += (e.State.Floor - o.Floor) * TimeBetweenFloors
+	} else if(e.State.Direction == DirnDown && o.Floor < e.State.Floor){
+		cost += 3 * numOrdersBelowToFloor(e, o.Floor)
+		cost += (e.State.Floor - o.Floor) * TimeBetweenFloors
+	} else if(e.State.Direction == DirnUp && o.Floor > e.State.Floor){ 
+		cost += 3 * numOrdersAboveToFloor(e, o.Floor)
+		cost += (o.Floor - e.State.Floor ) * TimeBetweenFloors
+	}
+	return float64(cost)
 }
 
 func OrderAssigner(id string, o Order, elevs Elevators) string {
@@ -40,5 +70,6 @@ func OrderAssigner(id string, o Order, elevs Elevators) string {
 			eCost = iCost
 		}
 	}
+	fmt.Printf("It will take %g seconds to reach floor %d",eCost, o.Floor)
 	return assignedId
 }
